7060dd3186eedcc90467087deacdb865aadb20a7
Yusuf Khan <22116082+kkyusufk@users.noreply.github.com>
2022-12-19 01:39:15 +0530

    added deployed URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
       mid: null,
       section: 'Welcome Screen'
     };
-    this.url = 'http://localhost:8000/phantom/onboarding/';
+    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom/onboarding';
     this.success = (status) => successCallback(status);
   }
 
