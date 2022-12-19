deff99d75063b3f28fdb159768f7180fbac8cfcc
Yusuf Khan <22116082+kkyusufk@users.noreply.github.com>
2022-12-19 17:43:51 +0530

    testing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
       mid: null,
       section: 'Welcome Screen'
     };
-    this.url = 'https://localhost:8000/phantom?partnerId=JFzhDzLxXOgAxM';
+    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom?partnerId=JFzhDzLxXOgAxM';
     this.success = (status) => successCallback(status);
   }
 
--- main.js
+++ main.js
@@ -100,7 +100,7 @@ var PartnerOnboarding = /*#__PURE__*/function () {
       mid: null,
       section: 'Welcome Screen'
     };
-    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom/onboarding';
+    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom?partnerId=JFzhDzLxXOgAxM';
     this.success = function (status) {
       return successCallback(status);
     };
