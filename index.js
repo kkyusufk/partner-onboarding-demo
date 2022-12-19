5cb566454d37817167df8c068a2c156327066726
Yusuf Khan <22116082+kkyusufk@users.noreply.github.com>
2022-12-19 16:59:38 +0530

    updating the link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
       mid: null,
       section: 'Welcome Screen'
     };
-    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom/onboarding';
+    this.url = 'https://localhost:8000/phantom?partnerId=JFzhDzLxXOgAxM';
     this.success = (status) => successCallback(status);
   }
 
