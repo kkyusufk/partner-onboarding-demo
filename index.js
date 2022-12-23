71857ed661fc33b2583f88762c3e5759e57179cf
Yusuf Khan <22116082+kkyusufk@users.noreply.github.com>
2022-12-24 00:22:35 +0530

    adding devstack

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
       mid: null,
       section: 'Welcome Screen'
     };
-    this.url = 'https://sme-dashboard.dev.razorpay.in/phantom';
+    this.url = 'https://sme-dashboard-phantom-demo.dev.razorpay.in/phantom';
     this.success = (status) => successCallback(status);
   }
 
